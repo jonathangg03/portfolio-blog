d9e16d280d09effa4d31f9811d8e0b324c602d1f
Jonathan García <jona03g97@gmail.com>
2022-08-25 22:18:49 -0600

    Improve: Habilities

--- components/Habilities/index.js
+++ components/Habilities/index.js
@@ -1,15 +1,33 @@
-import Subtitle from "../Subtitle"
+import Subtitle from '../Subtitle'
 import componentStyles from '../../styles/Components.module.css'
-import Hability from "../Hability"
+import Hability from '../Hability'
+import { FaReact, FaNodeJs, FaLaptopCode, FaGitAlt } from 'react-icons/fa'
 import styles from '../../styles/Habilities.module.css'
 
 const Habilities = () => {
-  return <section className={componentStyles.section_spacer}>
-    <Subtitle subtitle='Habilidades' />
-    <div className={styles.habilities_container}>
-      <Hability />
-    </div>
-  </section>
+  return (
+    <section className={componentStyles.section_spacer}>
+      <Subtitle subtitle='Habilidades' />
+      <div className={styles.habilities_container}>
+        <Hability
+          number='0'
+          Icon={FaReact}
+          name='Desarrollo de Single Page Applications'
+        />
+        <Hability number='1' Icon={FaNodeJs} name='Creación de APIs REST' />
+        <Hability
+          number='2'
+          Icon={FaLaptopCode}
+          name='Creación de Landing Pages'
+        />
+        <Hability
+          number='3'
+          Icon={FaGitAlt}
+          name='Control de versiones con Git'
+        />
+      </div>
+    </section>
+  )
 }
 
 export default Habilities
--- components/Hability/index.js
+++ components/Hability/index.js
@@ -1,11 +1,15 @@
 import styles from '../../styles/Habilities.module.css'
 
-const Hability = () => {
+const Hability = ({ number, Icon, name }) => {
   return (
-    <div className={styles.hability}>
-      <h3 className={styles.name}>Creación de Landing Pages </h3>
-      <p>Creación de landing pages con HTML, CSS/SASS y JavaScript.</p>
-    </div >
+    <div className={styles.hability} number={number}>
+      <div className={styles.icon}>
+        <Icon />
+      </div>
+      <div className={styles.description}>
+        <h3 className={styles.name}>{name}</h3>
+      </div>
+    </div>
   )
 }
 
