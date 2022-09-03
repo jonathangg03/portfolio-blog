dcb5a8d05a2e4c60cbe55bcc0d1aac5449812565
Jonathan García <jona03g97@gmail.com>
2022-09-02 21:30:07 -0600

    Add: Tablet bp

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,56 +1,82 @@
+import { useState } from 'react'
+import { IoMdMenu } from 'react-icons/io'
 import styles from '../../styles/Header.module.css'
 import Link from 'next/link'
 
 const Header = () => {
+  const [menuVisibility, setMenuVisibility] = useState(false)
+
+  const handleMenu = (event) => {
+    setMenuVisibility(false)
+  }
+
   return (
     <header className={styles.header}>
-      <figure className={styles.logo__container}>
+      <div className={styles.logo__container}>
         <p className={styles.logo}>
           {'<'}
           <span>davdev</span>
           {'/>'}
         </p>
-      </figure>
-      <nav>
+      </div>
+      <nav className={styles.nav} visibility={menuVisibility ? 1 : 0}>
         <ul className={styles.menu}>
           <li>
             <Link href='/blog'>
-              <a>Blog</a>
+              <a onClick={handleMenu}>Blog</a>
             </Link>
             <div className={styles.selector}></div>
           </li>
           <li>
             <Link href='/'>
-              <a>Inicio</a>
+              <a onClick={handleMenu}>Inicio</a>
             </Link>
             <div className={styles.selector}></div>
           </li>
           <li>
-            <a href='#aboutme'>Sobre mi</a>
+            <a href='#aboutme' onClick={handleMenu}>
+              Sobre mi
+            </a>
             <div className={styles.selector}></div>
           </li>
           <li>
-            <a href='#habilities'>Habilidades</a>
+            <a href='#habilities' onClick={handleMenu}>
+              Habilidades
+            </a>
             <div className={styles.selector}></div>
           </li>
           <li>
-            <a href='#projects'>Proyectos</a>
+            <a href='#projects' onClick={handleMenu}>
+              Proyectos
+            </a>
             <div className={styles.selector}></div>
           </li>
           <li>
-            <a href='#videos'>Videos</a>
+            <a href='#videos' onClick={handleMenu}>
+              Videos
+            </a>
             <div className={styles.selector}></div>
           </li>
           <li>
-            <a href='#contact'>Contacto</a>
+            <a href='#contact' onClick={handleMenu}>
+              Contacto
+            </a>
             <div className={styles.selector}></div>
           </li>
           <li>
-            <a href='#'>Curriculum</a>
+            <a href='#' onClick={handleMenu}>
+              Curriculum
+            </a>
             <div className={styles.selector}></div>
           </li>
         </ul>
       </nav>
+      <button
+        className={styles.menuActivator}
+        onClick={() => setMenuVisibility((prev) => !prev)}
+      >
+        <IoMdMenu />
+      </button>
     </header>
   )
 }
