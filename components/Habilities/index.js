f01b397db11469805226a528433e80d219c15c01
Jonathan García <jona03g97@gmail.com>
2022-09-05 21:36:59 -0600

    Change: Habilities

--- components/Habilities/index.js
+++ components/Habilities/index.js
@@ -22,25 +22,25 @@ const Habilities = () => {
             number='0'
             Icon={FaReact}
             name='Desarrollo con React.js'
-            description='Desarrollo de Single Page Applications y PWA tanto con React.js como con Next.js.'
+            description='Desarrollo de SPA y PWA tanto con React.js como con Next.js haciendo uso de hooks, Context API, CSS-in-JS y mucho más. Además de crear un entorno de desarrollo desde cero con Webpack.'
           />
           <Hability
             number='1'
             Icon={FaNodeJs}
             name='Creación de APIs REST'
-            description='Programación de APIs REST con Node.js y Express.js.'
+            description='Programación de APIs REST con Node.js y Express.js, que consulten bases de datos tanto relacionales como no relacionales.'
           />
           <Hability
             number='2'
             Icon={FaLaptopCode}
             name='Creación de Landing Pages'
-            description='Creación de Landing Pages con HTML, CSS/SASS y JavaScript.'
+            description='Creación de Landing Pages o Aplicaciones web con HTML, CSS o SASS para el estilizado, y JavaScript para la interacción. Control de la asincronía en JavaScript con la API Fetch así como manipulación del DOM.'
           />
           <Hability
             number='3'
             Icon={FaGitAlt}
             name='Control de versiones con Git'
-            description='Control de versiones con Git y manejo de repositorios con Github.'
+            description='Control de versiones con Git y manejo de repositorios con Github, hasta poder desplegar un sitio web en Vercel, Heroku, y más.'
           />
         </div>
       </div>
