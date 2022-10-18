1181b5c27a8e91b989558a949cbdaebaae2bbcec
Jonathan García <jona03g97@gmail.com>
2022-10-17 22:04:47 -0600

    Add: Entry component

--- components/Entry-Components/index.js
+++ components/Entry-Components/index.js
@@ -1,21 +1,41 @@
-const Title = ({ children }) => <h1 className="entry__title">{children}</h1>
-const SubTitles = ({ children }) => <h2 className="entry__subtitle">{children}</h2>
-const SectionTitle = ({ children }) => <h3 className="entry__section-title">{children}</h3>
-const Paragraph = ({ children }) => <p className="entry__paragraph">{children}</p>
-const Link = ({ children }) => <a href="/sdfd" className="entry__link">{children}</a>
-const ImageVertical = ({ src }) => <img src={src} className="vertical image" />
-const ImageHorizontal = ({ src }) => <img src={src} className="horizontal image" />
-const ImageSquare = ({ src }) => <img src={src} className="square image" />
-const Video = ({ src }) => <video src={{ src }} className="entry__video" />
+const Title = ({ children }) => <h1 className='entry__title'>{children}</h1>
 
-export {
+const SubTitle = ({ children }) => (
+  <h2 className='entry__subtitle'>{children}</h2>
+)
+
+const SectionTitle = ({ children }) => (
+  <h3 className='entry__section-title'>{children}</h3>
+)
+
+const Paragraph = ({ children }) => (
+  <p className='entry__paragraph'>{children}</p>
+)
+
+const Link = ({ children }) => (
+  <a href='/sdfd' className='entry__link'>
+    {children}
+  </a>
+)
+
+const ImageVertical = ({ src }) => <img src={src} className='vertical image' />
+
+const ImageHorizontal = ({ src }) => (
+  <img src={src} className='horizontal image' />
+)
+
+const ImageSquare = ({ src }) => <img src={src} className='square image' />
+
+const Video = ({ src }) => <video src={{ src }} className='entry__video' />
+
+export default {
   Title,
-  SubTitles,
+  SubTitle,
   SectionTitle,
   Paragraph,
   Link,
   ImageVertical,
   ImageHorizontal,
   ImageSquare,
-  Video,
+  Video
 }
--- pages/blog/[slug]/index.js
+++ pages/blog/[slug]/index.js
@@ -1,6 +1,8 @@
 import Header from '../../../components/Header'
-import EntryContent from '../../../components/EntryContent'
 import Footer from '../../../components/Footer'
+import { getFiles, getFileBySlug } from '../../../lib/mdx-reader'
+import EntryComponents from '../../../components/Entry-Components'
+import { MDXRemote } from 'next-mdx-remote'
 
 const OPTONS = [
   {
@@ -13,15 +15,40 @@ const OPTONS = [
   }
 ]
 
-
-const EntryPost = () => {
+const EntryPost = ({ source }) => {
   return (
     <>
       <Header options={OPTONS} />
-      <EntryContent />
+      <section className='section_spacer entry__body'>
+        <MDXRemote {...source} components={EntryComponents} />
+      </section>
       <Footer />
     </>
   )
 }
 
 export default EntryPost
+
+export async function getStaticProps({ params }) {
+  console.log('slug: - ', params.slug)
+  const { source, frontmatter } = await getFileBySlug({ slug: params.slug })
+
+  return {
+    props: {
+      source,
+      frontmatter
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getFiles()
+  const paths = posts.map((post) => ({
+    params: { slug: post.replace(/\.mdx/, '') }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
