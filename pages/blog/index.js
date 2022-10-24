5faea8f64bb96d758f3a19f1c55e15aebad3e88e
Jonathan Garcia <jona03g97@gmail.com>
2022-10-24 07:56:50 -0600

    Fix: No entries styles

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -28,28 +28,33 @@ const Blog = ({ entries, categories }) => {
         </p>
       </section>
       <section className='section_spacer'>
-        <h3 className='entries-title'>Recientes</h3>
-        <ul className='entries-list'>
-          {entries
-            .sort((current, post) => post.timestamp - current.timestamp)
-            .map((entry) => {
-              return (
-                <li className='entry' key={entry.slug}>
-                  <Link href={`/blog/${entry.slug}`}>
-                    <a>
-                      <h2>{entry.title}</h2>
-                      <p className='entry__description'>{entry.description}</p>
-                      <div className='line-animation top'></div>
-                      <div className='line-animation left'></div>
-                      <div className='line-animation right'></div>
-                      <div className='line-animation bottom'></div>
-                      <p className='entry__date'>{entry.date}</p>
-                    </a>
-                  </Link>
-                </li>
-              )
-            })}
-        </ul>
+        {
+          entries.length > 0 &&
+          <>
+            <h3 className='entries-title'>Recientes</h3>
+            <ul className='entries-list'>
+              {entries
+                .sort((current, post) => post.timestamp - current.timestamp)
+                .map((entry) => {
+                  return (
+                    <li className='entry' key={entry.slug}>
+                      <Link href={`/blog/${entry.slug}`}>
+                        <a>
+                          <h2>{entry.title}</h2>
+                          <p className='entry__description'>{entry.description}</p>
+                          <div className='line-animation top'></div>
+                          <div className='line-animation left'></div>
+                          <div className='line-animation right'></div>
+                          <div className='line-animation bottom'></div>
+                          <p className='entry__date'>{entry.date}</p>
+                        </a>
+                      </Link>
+                    </li>
+                  )
+                })}
+            </ul>
+          </>
+        }
         {categories.map((category) => (
           <EntriesList
             key={category}
